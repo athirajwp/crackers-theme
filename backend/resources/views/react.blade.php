2619f92f88d0271e9120a58970524b27948be776
athirajwp <athiraj.vnr@gmail.com>
2026-07-14 21:25:08 +0530

    build: compile assets after removing CartFooter details

--- backend/resources/views/react.blade.php
+++ backend/resources/views/react.blade.php
@@ -153,7 +153,7 @@
         100% { transform: translateX(-100%); }
       }
     </style>
-    <script type="module" crossorigin src="/build/assets/index-N9hXEn8V.js"></script>
+    <script type="module" crossorigin src="/build/assets/index-B6teDzNz.js"></script>
     <link rel="stylesheet" crossorigin href="/build/assets/index-CrlWzO9F.css">
   </head>
   <body class="bg-slate-50 text-slate-800 font-sans">
